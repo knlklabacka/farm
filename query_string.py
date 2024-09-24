18500dc092eec3bef82382628ba53a5defe5e3e4
knlklabacka <ATSIntranet+\knlklabacka@users.noreply.github.com>
2024-09-24 12:01:08 -0500

    updated query_string.py and test.http

--- query_string.py
+++ query_string.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from typing import Dict
+
+from fastapi import FastAPI, Path, Body
 
 app = FastAPI()
 
-@app.get("/cars/price")
-async def cars_by_price(min_price: int=0, max_price: int=100000):
-    return {"Message":f"Listing cars with prices between {min_price} and {max_price}"}
 
+@app.get("/cars/price")
+async def cars_by_price(min_price: int = 0, max_price: int = 10000):
+    return {"Message": f"Listing cars with prices between {min_price} and {max_price}"}
+
+
+@app.post("/cars")
+async def new_car(data: Dict = Body(...)):
+    print(data)
+    return {"message": data}
